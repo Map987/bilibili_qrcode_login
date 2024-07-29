e3191c0942a3ad79eca0c5c577a4ae3f0d2508ec
Map987 <99967856+Map987@users.noreply.github.com>
2024-07-29 13:12:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+import os
 import login
 import utils
 
+def get_script_dir():
+    """Get the directory of the current script."""
+    return os.path.dirname(os.path.abspath(__file__))
 
 if __name__ == '__main__':
-    # print(login.get_cookie().keys())
-    utils.save_json_file(path="./cookie.json", data=login.get_cookie())
+    script_dir = get_script_dir()
+    cookie_path = os.path.join(script_dir, "cookie.json")
 
+    # Save the cookie to a file in the same directory as the script
+    utils.save_json_file(path=cookie_path, data=login.get_cookie())
